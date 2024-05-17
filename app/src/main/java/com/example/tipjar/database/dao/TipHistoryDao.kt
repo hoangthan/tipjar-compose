731976fe143c76b784627731d3e5caac8adaba57
Steve <hoangthan345@gmail.com>
2024-05-18 01:46:32 +0700

    Add: Init database

--- app/src/main/java/com/example/tipjar/database/dao/TipHistoryDao.kt
+++ app/src/main/java/com/example/tipjar/database/dao/TipHistoryDao.kt
@@ -1,6 +1,18 @@
 package com.example.tipjar.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.tipjar.database.entity.TipHistory
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface TipHistoryDao
+interface TipHistoryDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(tipHistory: TipHistory)
+
+    @Query("SELECT * FROM tip_history")
+    fun getAll(): Flow<List<TipHistory>>
+}
--- app/src/main/java/com/example/tipjar/database/entity/TipHistory.kt
+++ app/src/main/java/com/example/tipjar/database/entity/TipHistory.kt
@@ -2,9 +2,15 @@ package com.example.tipjar.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "tip_history", primaryKeys = ["timestamp"])
+@Entity(tableName = "tip_history")
 data class TipHistory(
-    @ColumnInfo(name = "timestamp") val timestamp: Long
+    @PrimaryKey @ColumnInfo(name = "timestamp") val timestamp: Long,
 
+    @ColumnInfo(name = "bill_amount") val billAmount: Double,
+
+    @ColumnInfo(name = "tip_amount") val tipAmount: Double,
+
+    @ColumnInfo(name = "image_url") val imageUrl: String? = null
 )
